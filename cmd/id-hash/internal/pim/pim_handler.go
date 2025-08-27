4a0a861a2e59bdf178205a9fd183bb8190b78d0d
madalv <83772261+madalv@users.noreply.github.com>
2025-08-27 18:38:13 +0300

    more descriptive err msg

--- cmd/id-hash/internal/pim/pim_handler.go
+++ cmd/id-hash/internal/pim/pim_handler.go
@@ -86,7 +86,7 @@ func HandlerProcessMsisdnRequest(ctx *fasthttp.RequestCtx) {
 	}
 
 	if resp.StatusCode() != fasthttp.StatusNoContent {
-		ctx.Error(fmt.Sprintf("UID Mapper returned unexpected status: %d", resp.StatusCode()), fasthttp.StatusInternalServerError)
+		ctx.Error(fmt.Sprintf("UID Mapper returned unexpected status %d: %v", resp.StatusCode(), resp.Body()), fasthttp.StatusInternalServerError)
 		return
 	}
 
